96936d0e72873e2f69b99e3d0bdbe83927d0754a
Pardhasaradhi Gubbala <120232300+pardhasaradhi042@users.noreply.github.com>
2025-10-31 20:28:37 +0530

    Create Project:Tip_Calculator.py
    Day 2 Project: Tip Calculator
    Introduction to the Tip Calculator Project
    Welcome to the final project of today. In this project, we are going to build a Tip Calculator. The final output will look like this:
    
    It will print, "Welcome to the tip calculator." Then it will ask for the total bill amount.
    
    For example, if you and a couple of friends went out for lunch and the bill came to $124.56, it will then ask, "What percentage tip would you like to give?"
    
    If you want to give a 12% tip, you enter 12 and hit Enter. Finally, it will ask, "How many people are splitting this bill?"
    
    Let's say there are seven people; after entering this, the program will tell us that each person should pay about $19.93.
    
    Notice that even though the calculations might produce numbers with many decimal places, the final amount is rounded to two decimal places for accuracy.

---
+++ Day 2/Project:Tip_Calculator.py
@@ -0,0 +1,8 @@
+#Tip_calculator
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+split=(bill/people)*1.12
+print("Each person should pay: ",(round(split,2)))
